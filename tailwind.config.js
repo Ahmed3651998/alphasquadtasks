00a1eda130a3b8050c7224b9c42955fd9cf53ec3
Ahmed3651998 <aismail.bese17seecs@seecs.edu.pk>
2022-06-30 21:27:46 +0500

    breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
     ],
     theme: {
         extend: {},
+        screens: {
+            'sm': { 'min': '640px', 'max': '767px' },
+            'md': { 'min': '768px', 'max': '1023px' },
+            'lg': { 'min': '1024px', 'max': '1279px' },
+            'xl': { 'min': '1280px' },
+        },
     },
     plugins: [],
 }
